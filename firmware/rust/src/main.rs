7ceba41eb9a454a461ec343028fb4c2f30b20233
Nick Brown <nworbn@gmail.com>
2025-02-21 12:20:12 -0800

    dependency changes

--- firmware/rust/src/main.rs
+++ firmware/rust/src/main.rs
@@ -21,8 +21,7 @@ fn main() -> ! {
     pins.d9.into_floating_input();
     pins.d5.into_floating_input();
 
-    let mut display = hcms_29xx::Hcms29xx::new(
-        NUM_CHARS,                                // Number of characters in the display
+    let mut display = hcms_29xx::Hcms29xx::<_, NUM_CHARS>::new(
         pins.d0.into_output().downgrade(),        // Data pin
         pins.d1.into_output().downgrade(),        // Clock pin
         pins.d11.into_output().downgrade(),       // Chip select pin
