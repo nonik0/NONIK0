2e71fd08959f8974c34d1906209d8e91cbd33045
Nick Brown <nick@altonimb.us>
2025-05-06 16:37:47 -0700

    sensor fixes

--- firmware/rust/src/input.rs
+++ firmware/rust/src/input.rs
@@ -1,7 +1,7 @@
 use embedded_hal::digital::InputPin;
 
 const PRESS_CYCLES: u8 = 1;
-const HOLD_CYCLES: u8 = 10;
+const HOLD_CYCLES: u8 = 20;
 const DEBOUNCE_CYCLES: u8 = 3;
 
 pub enum InputEvent {
